f1404e0bcec50692dc9103f0a14ab263b929f5dd
Sebastian Urbina <sebita.urbina@gmail.com>
2017-10-29 01:08:52 -0300

    feat(server): enable CORS for local development

--- server/app/App.py
+++ server/app/App.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from server.api.Api import init_api
 
 app = Flask(__name__)
+# Allow CORS for local development
+cors = CORS(app, resources={ r"/api/*": {"origins": [ "http://localhost:3000" ]} })
 init_api(app)
